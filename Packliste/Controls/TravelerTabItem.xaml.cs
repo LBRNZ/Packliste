8b86629c2c162c44746e5f3e6ffcd3eb3b9e5094
Marc <microsoft@lbrnz.de>
2022-04-05 20:53:00 +0200

    Created TabItems for travelers

---
+++ Packliste/Controls/TravelerTabItem.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Packliste.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Packliste.Controls
+{
+    /// <summary>
+    /// Interaktionslogik für TravelerTabItem.xaml
+    /// </summary>
+    public partial class TravelerTabItem : TabItem, INotifyPropertyChanged
+    {
+
+
+        public Traveler Traveler
+        {
+            get { return (Traveler)GetValue(TravelerProperty); }
+            set 
+            {
+                SetValue(TravelerProperty, value); 
+                NotifyPropertyChanged();
+            }
+        }
+
+        public static readonly DependencyProperty TravelerProperty =
+            DependencyProperty.Register("Traveler", typeof(Traveler), typeof(TravelerTabItem));
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public TravelerTabItem()
+        {
+            InitializeComponent();
+            this.PropertyChanged += TravelerTabItem_PropertyChanged;
+        }
+
+        private void TravelerTabItem_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Traveler")
+            {
+                Header = Traveler.Person.Name;
+            }
+        }
+    }
+}
--- Packliste/Pages/PackinglistPage.xaml.cs
+++ Packliste/Pages/PackinglistPage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Packliste.Data;
+using Packliste.Controls;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +22,47 @@ namespace Packliste.Pages
     /// <summary>
     /// Interaktionslogik für PackinglistPage.xaml
     /// </summary>
-    public partial class PackinglistPage : Page
+    public partial class PackinglistPage : Page, INotifyPropertyChanged
     {
+        private XmlData _Data;
+
+        public XmlData Data
+        {
+            get { return _Data; }
+            set
+            {
+                _Data = value;
+                NotifyPropertyChanged();
+            }
+        }
         public PackinglistPage()
         {
             InitializeComponent();
+            Data = (Application.Current.MainWindow as MainWindow).data;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void JourneySelector_cb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            PersonsTabControl.Items.Clear();
+            Journey journey = ((ComboBox)sender).SelectedItem as Journey;
+            if (journey != null)
+            {
+                foreach (Traveler traveler in journey.Travelers)
+                {
+                    TravelerTabItem tabItem = new TravelerTabItem()
+                    {
+                        Traveler = traveler
+                    };
+                    PersonsTabControl.Items.Add(tabItem);
+                }
+            }
+
         }
     }
 }
